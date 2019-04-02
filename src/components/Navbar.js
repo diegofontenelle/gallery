8a05c08be6c8d9b936171307a044f835f8363d00
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2019-04-02 18:14:36 -0300

    Changing favicon and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,7 @@ const Navbar = ({ onUploadClicked }) => {
     <div className="ui pointing menu">
       <div className="ui container">
         <div className="item">
-          <img
-            className="ui mini image"
-            alt="Lux Gallery"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStZEDMGCggYg7HY6L3vjac6JuYeNr9QymdQeDHS5axwvFoNU-T"
-          />
+          <img className="ui mini image" alt="Lux Gallery" src="logo.jpeg" />
         </div>
         <div className="right menu">
           <a onClick={onUploadClicked} className="ui item">
